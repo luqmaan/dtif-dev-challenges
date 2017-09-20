24a4b6c9affa63258595b2994e8981b673c788c7
Mateo Clarke <code@mateoclarke.com>
2017-09-20 14:10:10 -0500

    add further starting point with jQuery

--- frontend/agnostic/index.js
+++ frontend/agnostic/index.js
@@ -7,3 +7,22 @@ $.ajax({
 }).done(function(data) {
   console.log(data);
 });
+
+// TODO: replace the static data with API queried data
+$.getJSON( "example.json", function( data ) {
+  var items = [];
+  $.each( data, function( i, dog ) {
+    items.push(
+      "<ul>" +
+        "<li>" + dog.description_of_dog + "</li>" +
+        "<li>" + "Owned by: " + dog.first_name + " " + dog.last_name + "</li>" +
+        "<li>" + "Lives at: " + dog.location_zip + "</li>" +
+      "</ul>"
+    );
+  });
+
+  $( "<div/>", {
+    "class": "dog-list",
+    html: items.join( "" )
+  }).appendTo( ".main" );
+});

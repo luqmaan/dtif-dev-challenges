8c16201498d3594fab51704a5e32d58bb01dd925
Brian Smith <briaguya@gmail.com>
2017-09-21 04:44:13 -0500

    return to schema

--- backend/python/run.py
+++ backend/python/run.py
@@ -2,5 +2,14 @@ import graphene
 from schema import Query
 
 schema = graphene.Schema(query=Query)
-result = schema.execute('{ hello }')
-print(result.data)
+result = schema.execute('''
+{
+  numberRange(end:10)
+  evenNumbers(end:10)
+  primeNumbers(end:10)
+  numberRangeAsWords(end:10)
+  somethingFun
+}
+''')
+
+print result.data

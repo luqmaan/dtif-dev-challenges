04c1a245a95ec639aba06cd90f576b13c2d125a0
Brian Smith <briaguya@gmail.com>
2017-09-21 03:28:32 -0500

    numberrange works

--- backend/python/schema.py
+++ backend/python/schema.py
@@ -1,9 +1,9 @@
 import graphene
 
 class Query(graphene.ObjectType):
-    numberRange = graphene.List(graphene.Int, end=graphene.Int())
+    numberRange = graphene.List(graphene.Int, start=graphene.Int(default_value=0), end=graphene.Int())
 
-    def resolve_numberRange(self, args, end):
-        return [1, 2]
+    def resolve_numberRange(self, args, start, end):
+        return range(start, end)
 
 schema = graphene.Schema(query=Query)
